1e2e661d330fe8f2190185ca2fd81c022bc9a9e6
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-02 13:40:36 +0100

    Define style for the images so they don't overflow nor distort

---
+++ src/page-loader.js
@@ -0,0 +1,6 @@
+export default function loadPage() {
+  const main = document.createElement("main");
+  const bgImage = document.createElement("img");
+  bgImage.src = "../images/pexels-cottonbro-hero-6466.jpeg";
+  bgImage.alt = "plate with boiled eggs";
+}
