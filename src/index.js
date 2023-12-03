b7ed9dcb871be569c04e0197ef18b6327e4b4b41
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-03 13:53:20 +0100

    Create .js files for each tab

--- src/index.js
+++ src/index.js
@@ -7,4 +7,16 @@ loadPage();
  3- make the header div fade in when the nav bar is hovered
  4- align content
  
+
+
+
+ take the logic that creates the header and the main body and 
+ put it into a file named page loader
+
+ when the page first loads, it renders the content of the home file on the content-div
+
+
+ when one of the header items is clicked, the user is redirected to the appropriate tab
+
+
  */
