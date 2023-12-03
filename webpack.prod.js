e60ef5586e15a0cb3b6e4931a1090ee124c6726b
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-03 08:29:36 +0100

    Install image optimizers and configure them in .prod.js file

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,25 +2,27 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: ["style-loader", "css-loader"],
-  //     },
-  //     {
-  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-  //       loader: "file-loader",
-  //       type: "asset/resource",
-  //     },
-  //   ],
-  // },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: "Restaurant Page",
     }),
   ],
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: ["jpegtran", { progressive: true }],
+          },
+        },
+      }),
+    ],
+  },
 });
