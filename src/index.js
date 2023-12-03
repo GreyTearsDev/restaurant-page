cdf15505defe237a2c0529fe837169ec4ae49e0a
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-03 06:50:42 +0100

    Increase the hero-image's brightness

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import loadPage from "./page-loader";
-
 import "./style.css";
 
 loadPage();
+
+// to do
+/*
+ 1- optimize image size
+ 2- increase brightness of the image (could try filters)
+ 3- make the header div fade in when the nav bar is hovered
+ 4- align content
+ 
+ */
