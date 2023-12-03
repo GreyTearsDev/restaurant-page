dc1fd608a43088cf7818aafaf8f234ae62ef9d0e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-03 19:23:34 +0100

    Set up a factory function for creating the menu items

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,41 @@
+import mainImage from "./assets/images/cottonbro.jpeg";
+
 export default function loadMenu() {
-  const cange = document.createElement("p");
-  cange.textContent = "hoooooooooooooooooooo";
-  return cange;
+  const main = document.createElement("main");
+  const bgImage = document.createElement("img");
+
+  bgImage.src = mainImage;
+  bgImage.alt = "plate with boiled eggs";
+  bgImage.id = "hero-image";
+
+  main.appendChild(bgImage);
+  return main;
 }
+
+const menuItem = function createMenuItem(name, price, imageSrc) {
+  const imageElement = document.createElement("img");
+  const nameElement = document.createElement("p");
+  const priceElement = document.createElement("h4");
+  const namePriceContainer = document.createElement("div");
+  const itemBody = document.createElement("div");
+
+  imageElement.src = imageSrc;
+  imageElement.classList = "blured";
+  imageElement.style.filter = "blur(5px)";
+
+  nameElement.textContent = name;
+  nameElement.className = "item-name";
+  priceElement.textContent = price;
+  priceElement.className = "item-price";
+
+  namePriceContainer.className = "name-price-container";
+  namePriceContainer.appendChild(nameElement);
+  namePriceContainer.appendChild(priceElement);
+
+  itemBody.appendChild(imageElement);
+  itemBody.appendChild(namePriceContainer);
+
+  return { imageElement, nameElement, priceElement };
+};
+
+const test = menuItem("me", 20, mainImage);
